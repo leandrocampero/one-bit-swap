b07873ed52580716a71e9d0f6e500769d42d9bf5
Esteban <juanet.17@gmail.com>
2022-10-04 22:57:13 -0300

    Separar el indice en dos box

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,30 @@
-import Layout from '@components/layout'
+import { Box, Grid } from '@mui/material'
+import { grey } from '@mui/material/colors'
+import styles from '@styles/layout.module.scss'
 
+const sxProps = {
+  mx: 2,
+  my: 5,
+  p: 5,
+  borderRadius: 2,
+  backgroundColor: grey[400],
+}
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Grid container>
+      <Grid item xs={8}>
+        <Box sx={sxProps} className={styles.base}>
+          {/* <Componente del gsus> */}
+          <h1>Ordenes Abiertas y demas</h1>
+
+          {/* </Componente del gsus> */}
+        </Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box sx={sxProps} className={styles.base}>
+          <h1>Ordenes compra/Venta e intercambio</h1>
+        </Box>
+      </Grid>
+    </Grid>
+  )
 }
