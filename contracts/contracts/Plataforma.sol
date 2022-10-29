1a59b0ab50907967266df58ee15a005d98ea411b
Leandro Campero <leandrocampero14@gmail.com>
2022-10-29 00:06:42 -0300

    Contrato base con método de prueba y formato de documentación

--- contracts/contracts/Plataforma.sol
+++ contracts/contracts/Plataforma.sol
@@ -1,29 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./Datos.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Plataforma {
-  address private propietario;
-  uint8 test;
-  event AccesoExitoso();
-
-  constructor(uint8 _test) {
-    propietario = msg.sender;
-    test = _test;
+contract Plataforma is Datos {
+  constructor(uint _montoMinimo) {
+    plataforma.propietario = msg.sender;
+    plataforma.montoMinimoUSD = _montoMinimo;
+    plataforma.estado = EstadoGeneral.ACTIVO;
   }
 
   /****************************************************************************/
 
-  modifier soloPropietario() {
-    require(msg.sender == propietario, "Solo el propietario puede acceder");
-    _;
-  }
-
-  /****************************************************************************/
-
-  function prueba() public soloPropietario {
-    emit AccesoExitoso();
+  /**
+   * @notice listar tokens que cumplan los criterios de búsqueda
+   * @param _ticker nombre del token a buscar (único en la blockchain)
+   * @param _soloActivos determina si solo se listan los tokens activos
+   * @return resultado array de tokens que cumplan los criterios de búsqueda
+   * @dev si ticker viene vacío, lista todo (dependiendo de si incluye solo activos)
+   */
+  function buscarTokens(string memory _ticker, bool _soloActivos)
+    public
+    view
+    returns (Token[] memory resultado)
+  {
+    Token memory token;
+    if (emptyString(_ticker)) {
+      token = tokensMap[_ticker];
+      require(token.existe, "El token no existe");
+      resultado[0] = token;
+    } else {
+      for (uint index = 0; index < tokensArray.length; index++) {
+        token = tokensMap[tokensArray[index]];
+        if (_soloActivos && token.estado == EstadoGeneral.ACTIVO) {
+          resultado[index] = token;
+        }
+      }
+    }
+    return resultado;
   }
 }
