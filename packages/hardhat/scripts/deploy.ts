90612715ee258400932b6f24e5b2267192539fe9
Leandro Campero <leandrocampero14@gmail.com>
2023-02-12 01:27:37 -0300

    feat(hardhat): exportar dirección del contrato desplegado para uso de la aplicación

--- packages/hardhat/scripts/deploy.ts
+++ packages/hardhat/scripts/deploy.ts
@@ -1,16 +1,26 @@
+import { writeFileSync } from 'fs'
 import { ethers } from 'hardhat'
 import 'typechain'
 
 async function main() {
   const args = 5
-  const Plataforma = await ethers.getContractFactory(
+  const PlataformaFactory = await ethers.getContractFactory(
     'contracts/Plataforma.sol:Plataforma'
   )
-  const despliegue = await Plataforma.deploy(args)
+  const plataforma = await PlataformaFactory.deploy(args)
 
-  await despliegue.deployed()
+  await plataforma.deployed()
 
-  console.log(`Contrato de plataforma desplegado en ${despliegue.address}`)
+  console.log(`Contrato de plataforma desplegado en ${plataforma.address}`)
+
+  const deploy = {
+    platform: plataforma.address,
+  }
+  writeFileSync(
+    `./../nextjs/src/contracts/deploy.json`,
+    JSON.stringify(deploy),
+    'utf-8'
+  )
 }
 
 // We recommend this pattern to be able to use async/await everywhere
