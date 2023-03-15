43d5f553f86a93a4076d92362a94aa4e4f90b8ac
Leandro Campero <leandrocampero14@gmail.com>
2023-03-15 12:01:38 -0300

    feat(nextjs): web3react hook

--- packages/nextjs/src/pages/_app.tsx
+++ packages/nextjs/src/pages/_app.tsx
@@ -1,16 +1,24 @@
 import Navbar from '@/components/navbar'
 import { BlockchainProvider } from '@/context/BlockchainProvider'
+import { Web3ReactProvider } from '@web3-react/core'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { Web3Provider } from '@ethersproject/providers'
+
+export function getLibrary(provider: any): Web3Provider {
+  const library = new Web3Provider(provider, 'any')
+  library.pollingInterval = 15000
+  return library
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <Web3ReactProvider getLibrary={getLibrary}>
       <BlockchainProvider>
         <Navbar />
         <Component {...pageProps} />
       </BlockchainProvider>
-    </div>
+    </Web3ReactProvider>
   )
 }
 
