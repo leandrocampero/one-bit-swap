23a29bb816ae96387f601fff81a747bf67b17936
Leandro Campero <leandrocampero14@gmail.com>
2023-03-15 21:27:16 -0300

    feat(nextjs): SessionProvider

--- packages/nextjs/src/pages/_app.tsx
+++ packages/nextjs/src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import Navbar from '@/components/navbar'
 import { BlockchainProvider } from '@/context/BlockchainProvider'
+import { SessionProvider } from '@/context/SessionProvider'
+import { Web3Provider } from '@ethersproject/providers'
 import { Web3ReactProvider } from '@web3-react/core'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import { Web3Provider } from '@ethersproject/providers'
 
 export function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider, 'any')
@@ -15,8 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <BlockchainProvider>
-        <Navbar />
-        <Component {...pageProps} />
+        <SessionProvider>
+          <Navbar />
+          <Component {...pageProps} />
+        </SessionProvider>
       </BlockchainProvider>
     </Web3ReactProvider>
   )
